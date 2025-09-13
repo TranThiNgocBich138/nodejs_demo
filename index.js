464fad1d768f819202c5372146218b2243057694
Tên của bạn <email_github@gmail.com>
2025-09-13 17:02:49 +0700

    Lần 1: Viết hàm hiển thị tên và tuổi

---
+++ index.js
@@ -0,0 +1,10 @@
+// Hàm hiển thị tên và tuổi
+function showInfo(name, age) {
+    console.log(`Tên: ${name}, Tuổi: ${age}`);
+}
+
+// Gọi thử hàm
+showInfo("Bình", 20);
+showInfo("Bích", 23);
+showInfo("Bảo", 22);
+showInfo("Bằng", 21);
